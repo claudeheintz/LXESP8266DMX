b9b1ca6267b4f90128acf45e7310baec33551f22
claudeheintz <dev@macluxpro.com>
2015-12-16 21:46:39 -0600

    cleanup

--- examples/DMXInputTest/DMXInputTest.ino
+++ examples/DMXInputTest/DMXInputTest.ino
@@ -16,7 +16,6 @@
 // LX8266DMXInput instance
 LX8266DMXInput* dmx_input = new LX8266DMXInput();
 
-uint8_t level = 0;
 int got_dmx = 0;
 
 void setup() {
@@ -36,13 +35,14 @@ void gotDMXCallback(int slots) {
 
 /************************************************************************
 
-  The main loop fades the levels of addresses 7 and 8 to full
+  The main loop checks to see if dmx input is available (got_dmx>0)
+  And then reads the level of dimmer 1 to set PWM level of LED connected to pin 14
   
 *************************************************************************/
 
 void loop() {
   if ( got_dmx ) {
-    //on ESP8266 PWM is 10bit 0-1024
+    //ESP8266 PWM is 10bit 0-1024
     analogWrite(14,2*dmx_input->getSlot(1));
   }
 }
